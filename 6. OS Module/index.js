ee6ed2af618a8d9c808e6a2fe1decad0dc553f48
martleru <68188509+martleru@users.noreply.github.com>
2024-07-22 21:09:01 +0300

    OS Module Basics

---
+++ 6. OS Module/index.js
@@ -0,0 +1,12 @@
+// OS or Operetaing System Module
+// Various commands that gives you information about OS, hover function to read description.
+
+import os from "os";
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.hostname());
+console.log(os.homedir());
+console.log(os.networkInterfaces());
+console.log(os.freemem());
